faac68bcff663bfbfd8c6174db3e1ea374da04ed
Rami Rustom <rustom@mit.edu>
2021-02-07 22:26:42 -0500

    finished write-tree file hashing

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # micro-git
 A micro git-like VCS in python
+
+Built while following [this amazing tutorial](https://www.leshenko.net/p/ugit/).
---
+++ ugit/base.py
@@ -0,0 +1,18 @@
+import os
+
+from . import data
+
+def write_tree(directory='.'):
+    with os.scandir(directory) as it:
+        for entry in it:
+            full = f'{directory}/{entry.name}'
+            if is_ignored(full):
+                continue
+            if entry.is_file(follow_symlinks=False):
+                with open(full, 'rb') as f:
+                    print(data.hash_object(f.read()), full)
+            elif entry.is_dir(follow_symlinks=False):
+                write_tree(full)
+
+def is_ignored(path):
+    return '.ugit' in path.split('/')
